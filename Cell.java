4508ab28040f66f7da251d985f1ec595d366c4be
MichaelfromSBS <Michael.Liu@sbs.org>
2024-02-16 03:25:49 -0500

    test

--- Cell.java
+++ Cell.java
@@ -1,2 +1,20 @@
-public class Cell{
+public class Cell {//Object class of cell which store whether it is empty or occupied by which player
+    private boolean isEmpty;//make instance variable private to prevent unwanted changes to information
+    private int playerNum;
+    public Cell(){//constructor of a cell with information initialized
+        isEmpty = true;
+        playerNum = -1;
+    }
+    public boolean getEmpty(){//accessor of isEmpty
+        return this.isEmpty;
+    }
+    public int getPlayerNum(){//accessor of playerNum
+        return this.playerNum;
+    }
+    public void setEmpty(boolean empty){//mutator of isEmpty
+        this.isEmpty = empty;
+    }
+    public void setPlayerNum(int num){//mutator of playerNum
+        this.playerNum = num;
+    }
 }
--- ConnectFour.java
+++ ConnectFour.java
@@ -1,123 +1,127 @@
-public class ConnectFour{
-
-  
-    private static Board board; //stores the Board used to play this game
-    private static Color[] colors; //stores the colors of each plyaer in order
-  
-public ConnectFour(){
-        addMouseListener(this);
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.*;
+public class ConnectFour extends JPanel implements MouseListener {//main method that play the game
+    public ConnectFour() {
+        addMouseListener(this); //always include this in the constructor!
     }
-  
-   public static void main(String[] args){
-     JFrame window = new JFrame("Connect Four");
-        window.setBounds(/*INSERT YOUR PARAMETERS HERE*/);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ConnectFour cf = new ConnectFour();
-        cf.setBackground(Color.WHITE);  // background color; the default color is light gray
-        Container c = window.getContentPane(); //gets the Window
-        c.add(cf); //adds the panel to the window
-        window.setVisible(true);
+    static int rows;//size of board
+    static int cols;
+    static int row_index;//where the element representing the place clicked
+    static int col_index;
+    static boolean drawing = false;//whether it's legal to draw a circle based on position
+    static Board game;//???
+    static int temp = 0;//index of player number array
+    static int player_number;
+    static int player[];//??? store player numbers
+    static int radius;//radius of a circle
+    static String colors[];//array to store colors for players
+    static int[][] circles; //array to keep track of all circle
+    public static void main(String[] args){//testing method
 
-     //More code will go in the main method here
-
-   }
-  
-//Simulates a virtual opponent. Implemented in part 5
-  public static void virtualOpponent(){
-
-  }
-  
-  public void paintComponent(Graphics g){
-            super.paintComponent(g);
-            board.draw(g, colors);      
-    }
-  
-      public void mousePressed(MouseEvent e) {}
-    public void mouseReleased(MouseEvent e) {}
-    public void mouseEntered(MouseEvent e) {}
-    public void mouseExited(MouseEvent e) {}
-    public void mouseClicked(MouseEvent e) {
-
-    }
-
-  public static String convertColorToString(Color col){
-        String color;
-        if (col.equals(Color.BLACK))
-            color = "Black";
-        else if(col.equals(Color.BLUE))
-            color = "Blue";
-        else if(col.equals(Color.CYAN))
-            color = "Cyan";
-        else if(col.equals(Color.DARK_GRAY))
-            color = "Dark Gray";
-        else if(col.equals(Color.GRAY))
-            color = "Gray";
-        else if(col.equals(Color.GREEN))
-            color = "Green";
-        else if(col.equals(Color.YELLOW))
-            color = "Yellow";
-        else if(col.equals(Color.LIGHT_GRAY))
-            color = "Light Gray";
-        else if(col.equals(Color.MAGENTA))
-            color = "LMagneta";
-        else if(col.equals(Color.ORANGE))
-            color = "Orange";
-        else if(col.equals(Color.PINK))
-            color = "Pink";
-        else if(col.equals(Color.RED))
-            color = "Red";
-        else if(col.equals(Color.WHITE))
-            color = "White";
-        else
-            color = "Black";
-        return color;
-    }
-    
-    public static Color convertStringToColor(String col){
-        Color color;
-        switch (col.toLowerCase()) {
-        case "black":
-            color = Color.BLACK;
-            break;
-        case "blue":
-            color = Color.BLUE;
-            break;
-        case "cyan":
-            color = Color.CYAN;
-            break;
-        case "darkgray":
-            color = Color.DARK_GRAY;
-            break;
-        case "gray":
-            color = Color.GRAY;
-            break;
-        case "green":
-            color = Color.GREEN;
-            break;
-        case "yellow":
-            color = Color.YELLOW;
-            break;
-        case "lightgray":
-            color = Color.LIGHT_GRAY;
-            break;
-        case "magenta":
-            color = Color.MAGENTA;
-            break;
-        case "orange":
-            color = Color.ORANGE;
-            break;
-        case "pink":
-            color = Color.PINK;
-            break;
-        case "red":
-            color = Color.RED;
-            break;
-        case "white":
-            color = Color.WHITE;
-            break;
-        default:
-            color = Color.BLACK;
+        Scanner user = new Scanner(System.in);
+        System.out.println("How many rows do you want?");
+        try{
+            rows = user.nextInt();
+        }
+        catch(Exception e){
+            while(!user.hasNextInt()){
+                System.out.println("Need a valid int number!");
+                user.nextLine();
             }
-        return color;
+            rows=user.nextInt();
+        }
+        System.out.println("How many columns do you want?");
+        try{
+            cols = user.nextInt();
+        }
+        catch(Exception e){
+            while(!user.hasNextInt()){
+                System.out.println("Need a valid int number!");
+                user.nextLine();
+            }
+            cols = user.nextInt();
+        }
+        circles = new int[rows*cols][2];
+        System.out.println("How many players are there?");
+        try{
+            player_number = user.nextInt();
+        }
+        catch(Exception e){
+            while(!user.hasNextInt()){
+                System.out.println("Need a valid int number!");
+                user.nextLine();
+            }
+            player_number  = user.nextInt();
+        }
+        player = new int[player_number];//array of player number, player 1 in index 0
+        colors = new String[player_number];//array of string of colors, player 1 in index 0
+        System.out.println("Please choose colors of players.");
+        for(int i = 0;i<player_number;i++){
+            System.out.println("Player " + (i+1) + ": ");
+            String temp = user.nextLine();
+            while(!temp.toLowerCase().equals("black")&&!temp.toLowerCase().equals("red")&&!temp.toLowerCase().equals("gray")&&!temp.toLowerCase().equals("orange")&&!temp.toLowerCase().equals("blue")&&!temp.toLowerCase().equals("green")&&!temp.toLowerCase().equals("yellow")&&!temp.toLowerCase().equals("white")){
+                System.out.println("Need a valid color!");
+                temp = user.nextLine();
+            }
+            colors[i]=temp.toLowerCase();
+            player[i]=i+1;
+        }
+
+        game = new Board(rows,cols);//???
+
+
+        JFrame window = new JFrame("Graphics Demo");
+        window.setBounds(200, 120, 1000, 800);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ConnectFour b = new ConnectFour();
+        b.setBackground(Color.WHITE);  // background color; the default color is light gray
+        Container c = window.getContentPane(); //gets the Window
+        c.add(b); //adds the panel to the window
+        window.setVisible(true);
+    }
+
+//creating the drawing panel
+
+    int index = 0;
+
+
+    public void paintComponent (Graphics g){//drawing horizontal and vertical lines to create grid
+        super.paintComponent(g); //usual
+
+        if(game.x_distance>=game.y_distance){
+            radius = (game.y_distance-10)/2;
+        }else{
+            radius = (game.x_distance-10)/2;
+        }
+        game.draw(g);
+
+    }
+
+    //These 4 are necessary for mouseListener, but you don't need to do anything with them
+    public void mousePressed(MouseEvent e) {
+    }
+    public void mouseReleased(MouseEvent e) {
+    }
+    public void mouseEntered(MouseEvent e) {
+    }
+    public void mouseExited(MouseEvent e) {
+    }
+
+    //Called whenever mouse is clicked
+    public void mouseClicked(MouseEvent e) {//determine situation after mouse is clicked
+        drawing = false;
+        Point p = e.getPoint(); //get the coordinates of the point where the mouse was clicked
+        row_index= (p.y-50)/game.y_distance;
+        col_index=(p.x-20)/game.x_distance;
+//        System.out.println(row_index+ " " + col_index);
+        if(row_index<rows&&col_index<cols){
+           if(game.selectcell(row_index,col_index,player[temp])){
+               drawing = true;
+               repaint();
+           }
+        }
     }
 }
